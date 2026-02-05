cf7ac61d0b6aea254c60b1e6c0746add032325d2
Ria Mundhra <rm2060@cam.ac.uk>
2026-02-05 14:57:17 +0800

    lint, dast,sast

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -4,3 +4,4 @@ docker pull zaproxy/zap-stable
 docker run -i zaproxy/zap-stable zap-baseline.py -t "https://kenken64.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
 
 echo $? > /dev/null
+
